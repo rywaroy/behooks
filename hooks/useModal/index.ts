a4fb11ddf1d6eeb2f92df882f99641acdf5e4542
rywaroy <464485682@qq.com>
2020-10-12 15:14:59 +0800

    feat: callback

--- hooks/useModal/index.ts
+++ hooks/useModal/index.ts
@@ -3,45 +3,72 @@ import useUpdateEffect from '../useUpdateEffect';
 import { UseAntdTableFormUtils } from '../useAntdTable';
 
 interface IOptions {
-    form?: UseAntdTableFormUtils | false;
-    defaultParams?: any;
+  form?: UseAntdTableFormUtils | false;
+  defaultParams?: any;
+  formatParams?: (values: any) => any;
+  submitCallback?: (values: any) => void;
+  openCallback?: () => void;
+  closeCallback?: () => void;
 }
 
 function useModal(service?: (params: any) => Promise<any>, options: IOptions = {}) {
   const [visible, setVisible] = useState<boolean>(false);
   const [key, setKey] = useState<number>(1);
 
-  const { form, defaultParams = {} } = options;
+  const {
+    form,
+    defaultParams = {},
+    formatParams,
+    submitCallback,
+    openCallback,
+    closeCallback,
+  } = options;
 
   const toggle = useCallback(() => {
-    // 关闭
     if (visible) {
+      // 关闭
       setVisible(false);
+      closeCallback && closeCallback();
     } else {
       // 打开
       setKey(Math.random());
+      openCallback && openCallback();
     }
   }, [visible, key]);
+
+  const submit = useCallback(() => {
+    let params = { ...defaultParams };
+    if (form) {
+      form.validateFields(null, (err, values) => {
+        if (!err) {
+          params = { ...params, ...values };
+          handleSubmit(params);
+        }
+      });
+    } else {
+      handleSubmit(params);
+    }
+  }, [toggle, form, service, defaultParams]);
+
+  const handleSubmit = useCallback((params) => {
+    if (formatParams) {
+      params = formatParams(params);
+    }
+    if (service) {
+      service(params)
+        .then((res) => {
+          submitCallback && submitCallback(res);
+          toggle();
+        });
+    } else {
+      submitCallback && submitCallback(params);
+      toggle();
+    }
+  }, [formatParams, service, submitCallback, toggle]);
 
   useUpdateEffect(() => {
     setVisible(true);
   }, [key]);
-
-  const submit = useCallback(() => {
-    if (service) {
-      let params = { ...defaultParams };
-      if (form) {
-        form.validateFields(null, (values) => {
-          params = { ...params, ...values };
-          service(params).then(() => toggle());
-        });
-      } else {
-        service(params).then(() => toggle());
-      }
-    } else {
-      toggle();
-    }
-  }, [toggle, form, service, defaultParams]);
 
   return {
     toggle,
