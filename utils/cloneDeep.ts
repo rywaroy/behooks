911b565d388438ca46c9be4aebcb9c20ff8276a9
rywaroy <464485682@qq.com>
2020-11-16 17:59:24 +0800

    style

--- utils/cloneDeep.ts
+++ utils/cloneDeep.ts
@@ -1,10 +1,8 @@
 export default function cloneDeep(target: any) {
   if (target && typeof target === 'object') {
     const isArray = Array.isArray(target);
-    const cloneTarget = isArray ? [] : {};
-
+    const cloneTarget: any = isArray ? [] : {};
     const keys = isArray ? target : Object.keys(target);
-
     for (let i = 0; i < keys.length; i++) {
       if (isArray) {
         cloneTarget[i] = cloneDeep(target[i]);
@@ -12,7 +10,6 @@ export default function cloneDeep(target: any) {
         cloneTarget[keys[i]] = cloneDeep(target[keys[i]]);
       }
     }
-
     return cloneTarget;
   }
   return target;
