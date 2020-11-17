81b1888e2b16a9dbcadac1dbf1fc0cc4ca3d8523
rywaroy <464485682@qq.com>
2020-11-17 10:34:23 +0800

    test: cloneDeep test

--- utils/cloneDeep.ts
+++ utils/cloneDeep.ts
@@ -1,16 +1,52 @@
-export default function cloneDeep(target: any) {
-  if (target && typeof target === 'object') {
-    const isArray = Array.isArray(target);
-    const cloneTarget: any = isArray ? [] : {};
-    const keys = isArray ? target : Object.keys(target);
-    for (let i = 0; i < keys.length; i++) {
-      if (isArray) {
-        cloneTarget[i] = cloneDeep(target[i]);
-      } else {
-        cloneTarget[keys[i]] = cloneDeep(target[keys[i]]);
-      }
-    }
-    return cloneTarget;
+interface IObject {
+  [prop: string]: any;
+}
+
+function cloneArray(target: any[]) {
+  const cloneTarget: any[] = [];
+  for (let i = 0; i < target.length; i++) {
+    cloneTarget[i] = cloneDeep(target[i]);
   }
-  return target;
+  return cloneTarget;
+}
+
+function cloneObject(target: IObject) {
+  const cloneTarget: IObject = {};
+  Object.keys(target).forEach((key) => {
+    cloneTarget[key] = cloneDeep(target[key]);
+  });
+  return cloneTarget;
+}
+
+function cloneDate(target: Date) {
+  return new Date(target.getTime());
+}
+
+function cloneRegExp(target: RegExp) {
+  return new RegExp(target);
+}
+
+export default function cloneDeep(target: any) {
+  const type = Object.prototype.toString.call(target);
+  let cloneTarget: any;
+
+  switch (type) {
+    case '[object Object]':
+      cloneTarget = cloneObject(target);
+      break;
+    case '[object Array]':
+      cloneTarget = cloneArray(target);
+      break;
+    case '[object Date]':
+      cloneTarget = cloneDate(target);
+      break;
+    case '[object RegExp]':
+      cloneTarget = cloneRegExp(target);
+      break;
+    default:
+      cloneTarget = target;
+      break;
+  }
+
+  return cloneTarget;
 }
